1a25e9a1808b57e5a07f88416fc072a588273c4c
Hamzah212 <133094712+Hamzah212@users.noreply.github.com>
2024-06-20 14:10:12 +0200

    Add files via upload

--- planets.blade.php
+++ planets.blade.php
@@ -5,13 +5,17 @@
 </head>
 <body>
     <h1>Planets</h1>
-    <ul>
-        @foreach ($planets as $planet)
-            <li>
-                <strong>{{ $planet['name'] }}</strong>
-                <p>{{ $planet['description'] }}</p>
-            </li>
-        @endforeach
-    </ul>
+    @if(count($planets) > 0)
+        <ul>
+            @foreach ($planets as $planet)
+                <li>
+                    <strong>{{ $planet['name'] }}</strong>
+                    <p>{{ $planet['description'] }}</p>
+                </li>
+            @endforeach
+        </ul>
+    @else
+        <p>No planets found.</p>
+    @endif
 </body>
 </html>
---
+++ web.php
@@ -0,0 +1,32 @@
+<?php
+Route::get('/planets', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    $planetName = request('planet');
+
+    if ($planetName) {
+        $planets = collect($planets)->filter(function ($planet) use ($planetName) {
+            return strtolower($planet['name']) === strtolower($planetName);
+        })->all();
+    }
+
+    return view('planets', ['planets' => $planets]);
+});
+?>
